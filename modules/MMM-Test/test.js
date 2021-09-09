ee4a08e5f44cafb33a3bbae3d032eefd1f9a5446
ltg0829 <71542896+ltg0829@users.noreply.github.com>
2021-09-09 22:34:16 +0900

    Update test.js

--- modules/MMM-Test/test.js
+++ modules/MMM-Test/test.js
@@ -7,4 +7,10 @@ Module.register('MMM-Test',{
   defaults:{
     lang:'kr',
     location:'seoul',
- 
+  }
+  start:function(){},
+  getDOm:function(){},
+  notificationReceived:function(){},
+  socketNotificationReceived:function(){},
+})
+
